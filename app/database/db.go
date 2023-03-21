59e2bef41f513da538d062c90be874f5fb7cf62f
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-21 16:59:18 +0700

    chore : db

---
+++ app/database/db.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB(dc config.DBConfig) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dc.DBUser, dc.DBPass, dc.DBHost, dc.DBPort, dc.DBName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println("database connection error : ", err.Error())
+		return nil
+	}
+
+	return db
+}
+
+// func Migrate(db *gorm.DB) {
+// 	db.AutoMigrate(
+// 		user.User{},
+// 		rooms.Room{},
+// 		reservations.Reservation{},
+// 		feedback.Feedback{},
+// 	)
+// }
