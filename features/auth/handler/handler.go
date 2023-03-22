d0899ac0c4417175b5a461a8c7fc53ca20476532
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-22 23:08:02 +0700

    feat : register in handler

---
+++ features/auth/handler/handler.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"musiclab-be/features/auth"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+type authControll struct {
+	srv auth.AuthService
+}
+
+func New(srv auth.AuthService) auth.AuthHandler {
+	return &authControll{
+		srv: srv,
+	}
+}
+
+// Register implements auth.AuthHandler
+func (ac *authControll) Register() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		input := RegisterRequest{}
+		err := c.Bind(&input)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "input format incorrect"})
+		}
+
+		err = ac.srv.Register(*ReqToCore(input))
+		if err != nil {
+			if strings.Contains(err.Error(), "already") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "email already registered"})
+			} else if strings.Contains(err.Error(), "is not min") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "validate: password length minimum 5 character"})
+			} else if strings.Contains(err.Error(), "validate") {
+				return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": err.Error()})
+			} else {
+				return c.JSON(http.StatusInternalServerError, map[string]interface{}{"message": "internal server error"})
+			}
+		}
+		return c.JSON(http.StatusCreated, map[string]interface{}{"message": "success create account"})
+
+	}
+}
+
+// Login implements auth.AuthHandler
+func (*authControll) Login() echo.HandlerFunc {
+	panic("unimplemented")
+}
--- go.mod
+++ go.mod
@@ -2,33 +2,32 @@ module musiclab-be
 
 go 1.19
 
-require github.com/spf13/viper v1.15.0
+require (
+	github.com/labstack/echo/v4 v4.9.0
+	github.com/spf13/viper v1.15.0
+	golang.org/x/crypto v0.7.0
+	gorm.io/gorm v1.23.8
+)
 
 require (
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/labstack/echo/v4 v4.9.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
-	github.com/leodido/go-urn v1.2.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	gorm.io/gorm v1.23.8 // indirect
 )
 
 require (
 	github.com/aws/aws-sdk-go v1.44.226
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/go-playground/validator/v10 v10.12.0
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/labstack/echo-jwt/v4 v4.1.0
