394a22267e98f5d2afd40d8671f3ea4548543318
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-23 09:46:17 +0700

    feat: create mentors model

--- features/mentors/data/model.go
+++ features/mentors/data/model.go
@@ -1,1 +1,18 @@
 package data
+
+import "gorm.io/gorm"
+
+type Mentor struct {
+	gorm.Model
+	Avatar    string
+	Name      string
+	Email     string `gorm:"unique"`
+	Password  string
+	Role      string `gorm:"type:varchar(25) not null default 'Mentor'"`
+	Sex       string
+	Phone     string `gorm:"type:varchar(12)"`
+	Address   string `gorm:"type:varchar(100)"`
+	Instagram string
+	Price     float64 `gorm:"type:float not null"`
+	AvgRating float32 `gorm:"type:float not null"`
+}
--- features/students/data/model.go
+++ features/students/data/model.go
@@ -8,7 +8,7 @@ type Student struct {
 	Name     string
 	Email    string `gorm:"unique"`
 	Password string
-	Role     string `gorm:"type:varchar(25) not null default 'student'"`
+	Role     string `gorm:"type:varchar(25) not null default 'Student'"`
 	Sex      string
 	Phone    string `gorm:"type:varchar(12)"`
 	Address  string `gorm:"type:varchar(100)"`
