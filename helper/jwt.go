114f3b5e38b942ec099a6b587e1ff0a02a0c6ebb
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-23 21:52:21 +0700

    middleware: create token by user id and role

--- helper/jwt.go
+++ helper/jwt.go
@@ -16,10 +16,11 @@ func JWTMiddleware() echo.MiddlewareFunc {
 	})
 }
 
-func CreateToken(userId uint) (string, error) {
+func CreateToken(userId uint, role string) (string, error) {
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
 	claims["userId"] = userId
+	claims["role"] = role
 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString([]byte(config.JWTKey))
@@ -33,4 +34,14 @@ func ExtractTokenUserId(e echo.Context) uint {
 		return uint(userId)
 	}
 	return 0
+}
+
+func ExtractTokenUserRole(e echo.Context) string {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		role := claims["role"].(string)
+		return role
+	}
+	return ""
 }
