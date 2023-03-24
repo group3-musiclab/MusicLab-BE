5c189e61973255ed6bd4a809ccaaaf499069b118
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-24 11:00:24 +0700

    refactor: query response

---
+++ utils/consts/query.go
@@ -0,0 +1,9 @@
+package consts
+
+// Error Response
+const (
+	QUERY_ErrorSelect     string = "error select data"
+	QUERY_NotFound        string = "data not found"
+	QUERY_ErrorInsertData string = "error insert data"
+	QUERY_NoRowsAffected  string = "row affected = 0"
+)
