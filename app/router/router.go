8983096a1cdb713346b89d50c5fccb7f3a10aa79
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-26 06:57:29 +0700

    chore : post class endpoint in router

--- app/router/router.go
+++ app/router/router.go
@@ -25,6 +25,10 @@ import (
 	reviewDataHdl "musiclab-be/features/reviews/handler"
 	reviewDataSrv "musiclab-be/features/reviews/services"
 
+	classData "musiclab-be/features/classes/data"
+	classDataHdl "musiclab-be/features/classes/handler"
+	classDataSrv "musiclab-be/features/classes/services"
+
 	"musiclab-be/utils/helper"
 
 	"github.com/labstack/echo/v4"
@@ -56,6 +60,10 @@ func InitRouter(db *gorm.DB, e *echo.Echo) {
 	rData := reviewData.New(db)
 	rSrv := reviewDataSrv.New(rData)
 	rHdl := reviewDataHdl.New(rSrv)
+
+	cData := classData.New(db)
+	cSrv := classDataSrv.New(cData)
+	cHdl := classDataHdl.New(cSrv)
 
 	// Auth
 	e.POST("/login", aHdl.Login())
@@ -92,5 +100,8 @@ func InitRouter(db *gorm.DB, e *echo.Echo) {
 	// Mentor Review
 	e.POST("/mentors/:mentor_id/reviews", rHdl.PostMentorReview(), helper.JWTMiddleware())
 	e.GET("/mentors/:mentor_id/reviews", rHdl.GetMentorReview(), helper.JWTMiddleware())
+
+	// Mentor Class
+	e.POST("/mentors/classes", cHdl.PostClass(), helper.JWTMiddleware())
 
 }
