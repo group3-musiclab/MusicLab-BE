32acf4c993c9e21c3d971e91d680c1493596c12c
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-26 02:38:57 +0700

    chore : entity interface

--- features/reviews/entity.go
+++ features/reviews/entity.go
@@ -1,9 +1,26 @@
 package reviews
 
+import "github.com/labstack/echo/v4"
+
 type Core struct {
 	ID        uint
 	MentorID  uint
 	StudentID uint
 	Rating    uint
 	Comment   string
+}
+
+type ReviewHandler interface {
+	PostMentorReview() echo.HandlerFunc
+	GetMentorReview() echo.HandlerFunc
+}
+
+type ReviewService interface {
+	PostMentorReview(newReview Core) error
+	GetMentorReview(mentorID uint) ([]Core, error)
+}
+
+type ReviewData interface {
+	PostMentorReview(newGenre Core) error
+	GetMentorReview(mentorID uint) ([]Core, error)
 }
