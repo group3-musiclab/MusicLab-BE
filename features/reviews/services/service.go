e3ce080ab73083a6b9e8c5baab853713eb4d4dad
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-26 02:57:27 +0700

    feat : post mentor review in service

--- features/reviews/services/service.go
+++ features/reviews/services/service.go
@@ -1,1 +1,13 @@
 package services
+
+import "musiclab-be/features/reviews"
+
+type reviewUseCase struct {
+	qry reviews.ReviewData
+}
+
+func New(rd reviews.ReviewData) reviews.ReviewService {
+	return &reviewUseCase{
+		qry: rd,
+	}
+}
