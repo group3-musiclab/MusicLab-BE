4f48ef8e220a831f2b6c4f3726da97e8d00d9900
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-26 06:35:49 +0700

    chore : add func toCore and datatoCore in class model

--- features/classes/data/model.go
+++ features/classes/data/model.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"musiclab-be/features/classes"
 	_modelTransaction "musiclab-be/features/transactions/data"
 
 	"gorm.io/gorm"
@@ -19,4 +20,36 @@ type Class struct {
 	Price        string `gorm:"type:float not null"`
 	Duration     uint
 	Transactions []_modelTransaction.Transaction
+}
+
+func ToCore(data Class) classes.Core {
+	return classes.Core{
+		ID:          data.ID,
+		MentorID:    data.MentorID,
+		Name:        data.Name,
+		Image:       data.Image,
+		Level:       data.Level,
+		Description: data.Description,
+		Syllabus:    data.Syllabus,
+		Requirement: data.Requirement,
+		ForWhom:     data.ForWhom,
+		Price:       data.Price,
+		Duration:    data.Duration,
+	}
+}
+
+func CoreToData(data classes.Core) Class {
+	return Class{
+		Model:       gorm.Model{ID: data.ID},
+		MentorID:    data.MentorID,
+		Name:        data.Name,
+		Image:       data.Image,
+		Level:       data.Level,
+		Description: data.Description,
+		Syllabus:    data.Syllabus,
+		Requirement: data.Requirement,
+		ForWhom:     data.ForWhom,
+		Price:       data.Price,
+		Duration:    data.Duration,
+	}
 }
