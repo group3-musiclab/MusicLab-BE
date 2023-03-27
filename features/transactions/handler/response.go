0288b33190144caa079ec10becfaa4f606661c94
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-27 23:12:40 +0700

    fix get mentor and student transaction response

--- features/transactions/handler/response.go
+++ features/transactions/handler/response.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"musiclab-be/features/transactions"
-	"time"
 )
 
 type TransactionResponse struct {
@@ -16,44 +15,48 @@ func TransactionResponseResponse(data transactions.Core) TransactionResponse {
 }
 
 type ShowAllMentorTransaction struct {
-	ID          uint      `json:"id"`
-	StudentName string    `json:"student_name"`
-	ClassName   string    `json:"class_name"`
-	StartDate   time.Time `json:"start_date"`
-	EndDate     time.Time `json:"end_date"`
-	Price       float64   `json:"price"`
-	Status      string    `json:"status"`
+	ID          uint    `json:"id"`
+	StudentName string  `json:"student_name"`
+	ClassName   string  `json:"class_name"`
+	StartDate   string  `json:"start_date"`
+	EndDate     string  `json:"end_date"`
+	Price       float64 `json:"price"`
+	Status      string  `json:"status"`
 }
 
 func ShowAllMentorTransactionResponse(data transactions.Core) ShowAllMentorTransaction {
+	StartDate := data.StartDate.Format("2006-01-02")
+	EndDate := data.EndDate.Format("2006-01-02")
 	return ShowAllMentorTransaction{
 		ID:          data.ID,
 		StudentName: data.Student.Name,
 		ClassName:   data.Class.Name,
-		StartDate:   data.StartDate,
-		EndDate:     data.EndDate,
+		StartDate:   StartDate,
+		EndDate:     EndDate,
 		Price:       data.Price,
 		Status:      data.Status,
 	}
 }
 
 type ShowAllStudentTransaction struct {
-	ID         uint      `json:"id"`
-	MentorName string    `json:"mentor_name"`
-	ClassName  string    `json:"class_name"`
-	StartDate  time.Time `json:"start_date"`
-	EndDate    time.Time `json:"end_date"`
-	Price      float64   `json:"price"`
-	Status     string    `json:"status"`
+	ID         uint    `json:"id"`
+	MentorName string  `json:"mentor_name"`
+	ClassName  string  `json:"class_name"`
+	StartDate  string  `json:"start_date"`
+	EndDate    string  `json:"end_date"`
+	Price      float64 `json:"price"`
+	Status     string  `json:"status"`
 }
 
 func ShowAllStudentTransactionResponse(data transactions.Core) ShowAllStudentTransaction {
+	StartDate := data.StartDate.Format("2006-01-02")
+	EndDate := data.EndDate.Format("2006-01-02")
 	return ShowAllStudentTransaction{
 		ID:         data.ID,
 		MentorName: data.Mentor.Name,
 		ClassName:  data.Class.Name,
-		StartDate:  data.StartDate,
-		EndDate:    data.EndDate,
+		StartDate:  StartDate,
+		EndDate:    EndDate,
 		Price:      data.Price,
 		Status:     data.Status,
 	}
