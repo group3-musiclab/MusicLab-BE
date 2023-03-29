8fc04366b44b8595240f187a20179c48841654ba
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-03-30 00:39:51 +0700

    test : genre 96.4%

---
+++ features/genres/services/service_test.go
@@ -0,0 +1,118 @@
+package services
+
+import (
+	"errors"
+	"musiclab-be/features/genres"
+	"musiclab-be/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestAddMentorGenre(t *testing.T) {
+	repo := mocks.NewGenreData(t)
+	inputData := genres.Core{Name: "Jazz", GenreID: 1}
+
+	t.Run("success add mentor book", func(t *testing.T) {
+		repo.On("AddMentorGenre", inputData).Return(nil).Once()
+		srv := New(repo)
+		err := srv.AddMentorGenre(inputData)
+		assert.Nil(t, err)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("genre not found", func(t *testing.T) {
+		repo.On("AddMentorGenre", mock.Anything).Return(errors.New("not found"))
+		srv := New(repo)
+		err := srv.AddMentorGenre(inputData)
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "not found")
+		repo.AssertExpectations(t)
+	})
+
+}
+
+func TestGetGenre(t *testing.T) {
+	repo := mocks.NewGenreData(t)
+	resData := []genres.Core{{ID: uint(1), Name: "Jazz"}}
+
+	t.Run("success get all genre", func(t *testing.T) {
+		repo.On("GetGenre").Return(resData, nil).Once()
+		srv := New(repo)
+		res, err := srv.GetGenre()
+		assert.Nil(t, err)
+		assert.Equal(t, len(resData), len(res))
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("genre not found", func(t *testing.T) {
+		repo.On("GetGenre").Return([]genres.Core{}, errors.New("genre not found")).Once()
+		srv := New(repo)
+		res, err := srv.GetGenre()
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "not found")
+		assert.Equal(t, 0, len(res))
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("problem with the server", func(t *testing.T) {
+		repo.On("GetGenre").Return([]genres.Core{}, errors.New("problem with the server")).Once()
+		srv := New(repo)
+		res, err := srv.GetGenre()
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "server")
+		assert.Equal(t, 0, len(res))
+		repo.AssertExpectations(t)
+	})
+
+}
+
+func TestGetMentorGenre(t *testing.T) {
+	repo := mocks.NewGenreData(t)
+	resData := []genres.MentorGenreCore{{ID: uint(1)}}
+
+	t.Run("success get mentor genre", func(t *testing.T) {
+		repo.On("GetMentorGenre", uint(1)).Return(resData, nil).Once()
+		srv := New(repo)
+		res, err := srv.GetMentorGenre(uint(1))
+		assert.Nil(t, err)
+		assert.Equal(t, len(resData), len(res))
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("problem with the server", func(t *testing.T) {
+		repo.On("GetMentorGenre", uint(1)).Return([]genres.MentorGenreCore{}, errors.New("problem with the server")).Once()
+		srv := New(repo)
+		res, err := srv.GetMentorGenre(uint(1))
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "server")
+		assert.Equal(t, 0, len(res))
+		repo.AssertExpectations(t)
+	})
+
+}
+
+func TestDelete(t *testing.T) {
+	repo := mocks.NewGenreData(t)
+
+	t.Run("success delete mentor genre", func(t *testing.T) {
+		repo.On("Delete", uint(1), uint(1)).Return(nil).Once()
+
+		srv := New(repo)
+		err := srv.Delete(uint(1), uint(1))
+		assert.Nil(t, err)
+		repo.AssertExpectations(t)
+
+	})
+
+	t.Run("problem with the server", func(t *testing.T) {
+		repo.On("Delete", uint(1), uint(1)).Return(errors.New("problem with the server")).Once()
+		srv := New(repo)
+		err := srv.Delete(uint(1), uint(1))
+		assert.NotNil(t, err)
+		assert.ErrorContains(t, err, "server")
+		repo.AssertExpectations(t)
+	})
+
+}
--- go.mod
+++ go.mod
@@ -3,16 +3,23 @@ module musiclab-be
 go 1.19
 
 require (
+	github.com/go-co-op/gocron v1.19.0
 	github.com/go-playground/validator/v10 v10.12.0
 	github.com/labstack/echo/v4 v4.9.0
+	github.com/lithammer/shortuuid/v4 v4.0.0
+	github.com/midtrans/midtrans-go v1.3.6
 	github.com/spf13/viper v1.15.0
+	github.com/stretchr/testify v1.8.2
 	golang.org/x/crypto v0.7.0
+	golang.org/x/oauth2 v0.6.0
+	google.golang.org/api v0.114.0
 	gorm.io/gorm v1.23.8
 )
 
 require (
 	cloud.google.com/go/compute v1.18.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
@@ -27,17 +34,17 @@ require (
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/leodido/go-urn v1.2.2 // indirect
-	github.com/lithammer/shortuuid/v4 v4.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/midtrans/midtrans-go v1.3.6 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	google.golang.org/api v0.114.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
 	google.golang.org/grpc v1.53.0 // indirect
