0074ae9a033cff3d45edefb7f0485e08570d5a0d
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-03-31 00:00:52 +0700

    fix: query param type for mentor filter

--- utils/consts/delivery.go
+++ utils/consts/delivery.go
@@ -2,12 +2,14 @@ package consts
 
 // Error Response
 const (
-	HANDLER_ErrorIdParam       string = "id param must number"
-	HANDLER_ErrorFormFile      string = "error form file"
-	HANDLER_ErrorBlobFile      string = "blob file not found"
-	HANDLER_InvalidPageParam   string = "invalid page parameter"
-	HANDLER_InvalidLimitParam  string = "invalid limit parameter"
-	HANDLER_InvalidIdStudent   string = "id student must number"
-	HANDLER_InvalidIdMentor    string = "id mentor must number"
-	HANDLER_InvalidRatingParam string = "invalid rating query param must number"
+	HANDLER_ErrorIdParam           string = "id param must number"
+	HANDLER_ErrorFormFile          string = "error form file"
+	HANDLER_ErrorBlobFile          string = "blob file not found"
+	HANDLER_InvalidPageParam       string = "invalid page parameter"
+	HANDLER_InvalidLimitParam      string = "invalid limit parameter"
+	HANDLER_InvalidIdStudent       string = "id student must number"
+	HANDLER_InvalidIdMentor        string = "id mentor must number"
+	HANDLER_InvalidRatingParam     string = "rating query param must number"
+	HANDLER_InvalidInstrumentParam string = "instrument query param must number"
+	HANDLER_InvalidGenreParam      string = "genre query param must number"
 )
