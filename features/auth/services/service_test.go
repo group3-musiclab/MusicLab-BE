11eb35a817052b7abf243de8400c5989f5665d88
Kharisma Januar Muhammad JN <kharisma.januar@gmail.com>
2023-04-01 15:36:21 +0700

    test: login auth 31%

--- features/auth/services/service_test.go
+++ features/auth/services/service_test.go
@@ -11,6 +11,18 @@ import (
 )
 
 var (
+	mock_mentor_core = auth.Core{
+		Name:     "Michael Jackson",
+		Email:    "mj@mail.com",
+		Password: "$2a$14$ZUTFIHo0iJEKuy7OcE0gge.RIgkU.iJjBum0E0QupmnZMRHMjjgQ.",
+		Role:     "Mentor",
+	}
+	mock_student_core = auth.Core{
+		Name:     "Bruno Mars",
+		Email:    "bruno@mail.com",
+		Password: "$2a$14$4blAj2MQJnMEdyB.Dd6LD.2e9KD75TOefD1VcLWSa4TqcxNi4kejy",
+		Role:     "Student",
+	}
 	mock_register_mentor = auth.Core{
 		Name:     "Michael Jackson",
 		Email:    "mj@mail.com",
@@ -21,6 +33,16 @@ var (
 		Name:     "Michael Jackson",
 		Email:    "mj@mail.com",
 		Password: "pop",
+		Role:     "Student",
+	}
+	mock_login_mentor = auth.Core{
+		Email:    "mj@mail.com",
+		Password: "pop",
+		Role:     "Mentor",
+	}
+	mock_login_student = auth.Core{
+		Email:    "bruno@mail.com",
+		Password: "mars",
 		Role:     "Student",
 	}
 )
@@ -128,4 +150,104 @@ func TestRegister(t *testing.T) {
 		repoAuth.AssertExpectations(t)
 	})
 
+}
+
+func TestLogin(t *testing.T) {
+	repoAuth := new(mocks.AuthData)
+	repoGoogle := new(mocks.GoogleAPI)
+	repoTransaction := new(mocks.TransactionData)
+	repoClass := new(mocks.ClassData)
+	repoStudent := new(mocks.StudentData)
+	repoSchedule := new(mocks.ScheduleData)
+	repoMentor := new(mocks.MentorData)
+
+	t.Run("Failed validate", func(t *testing.T) {
+		inputData := auth.Core{
+			Email: "mj@mail.com",
+		}
+		srv := New(repoAuth, repoGoogle, repoTransaction, repoClass, repoStudent, repoSchedule, repoMentor)
+		token, core, err := srv.Login(inputData)
+		assert.NotNil(t, err)
+		assert.Equal(t, "", token)
+		assert.Equal(t, auth.Core{}, core)
+		repoAuth.AssertExpectations(t)
+	})
+
+	t.Run("Login Mentor", func(t *testing.T) {
+		repoAuth.On("LoginMentor", mock.Anything).Return(mock_mentor_core, nil).Once()
+
+		srv := New(repoAuth, repoGoogle, repoTransaction, repoClass, repoStudent, repoSchedule, repoMentor)
+		token, core, err := srv.Login(mock_login_mentor)
+		assert.Nil(t, err)
+		assert.Equal(t, token, token)
+		assert.Equal(t, mock_mentor_core, core)
+		repoAuth.AssertExpectations(t)
+	})
+
+	t.Run("Failed Login Mentor", func(t *testing.T) {
+		repoAuth.On("LoginMentor", mock.Anything).Return(auth.Core{}, errors.New("error login")).Once()
+
+		srv := New(repoAuth, repoGoogle, repoTransaction, repoClass, repoStudent, repoSchedule, repoMentor)
+		token, core, err := srv.Login(mock_login_mentor)
+		assert.NotNil(t, err)
+		assert.Equal(t, "", token)
+		assert.Equal(t, auth.Core{}, core)
+		repoAuth.AssertExpectations(t)
+	})
+
+	t.Run("Login Student", func(t *testing.T) {
+		repoAuth.On("LoginStudent", mock.Anything).Return(mock_student_core, nil).Once()
+
+		srv := New(repoAuth, repoGoogle, repoTransaction, repoClass, repoStudent, repoSchedule, repoMentor)
+		token, core, err := srv.Login(mock_login_student)
+		assert.Nil(t, err)
+		assert.Equal(t, token, token)
+		assert.Equal(t, mock_student_core, core)
+		repoAuth.AssertExpectations(t)
+	})
+
+	t.Run("Failed Login Student", func(t *testing.T) {
+		repoAuth.On("LoginStudent", mock.Anything).Return(auth.Core{}, errors.New("error login")).Once()
+
+		srv := New(repoAuth, repoGoogle, repoTransaction, repoClass, repoStudent, repoSchedule, repoMentor)
+		token, core, err := srv.Login(mock_login_student)
+		assert.NotNil(t, err)
+		assert.Equal(t, "", token)
+		assert.Equal(t, auth.Core{}, core)
+		repoAuth.AssertExpectations(t)
+	})
+
+	t.Run("Failed Role", func(t *testing.T) {
+		inputData := auth.Core{
+			Name:     "Michael Jackson",
+			Email:    "mj@mail.com",
+			Password: "pop",
+			Role:     "Admin",
+		}
+
+		srv := New(repoAuth, repoGoogle, repoTransaction, repoClass, repoStudent, repoSchedule, repoMentor)
+		token, core, err := srv.Login(inputData)
+		assert.NotNil(t, err)
+		assert.Equal(t, "", token)
+		assert.Equal(t, auth.Core{}, core)
+		repoAuth.AssertExpectations(t)
+	})
+
+	t.Run("Failed Password", func(t *testing.T) {
+		inputData := auth.Core{
+			Email:    "bruno@mail.com",
+			Password: "bruno",
+			Role:     "Student",
+		}
+
+		repoAuth.On("LoginStudent", mock.Anything).Return(mock_student_core, nil).Once()
+
+		srv := New(repoAuth, repoGoogle, repoTransaction, repoClass, repoStudent, repoSchedule, repoMentor)
+		token, core, err := srv.Login(inputData)
+		assert.NotNil(t, err)
+		assert.Equal(t, "", token)
+		assert.Equal(t, auth.Core{}, core)
+		assert.Equal(t, "password not matched", err.Error())
+		repoAuth.AssertExpectations(t)
+	})
 }
