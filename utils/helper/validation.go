72f3d6c38b2f292ef7c93f4417865f9a57f25b92
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2023-04-03 05:24:43 +0700

    chore : delete unused function

--- utils/helper/validation.go
+++ utils/helper/validation.go
@@ -2,13 +2,9 @@ package helper
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"mime/multipart"
-	"musiclab-be/features/auth"
 	"net/http"
-
-	"github.com/go-playground/validator/v10"
 )
 
 func TypeFile(test multipart.File) (string, error) {
@@ -24,39 +20,4 @@ func TypeFile(test multipart.File) (string, error) {
 		return TipenamaFile, nil
 	}
 	return "", errors.New("file type not match")
-}
-
-type UserValidate struct {
-	Name     string `validate:"required"`
-	Email    string `validate:"required,email"`
-	Password string `validate:"required,min=3,alphanum"`
-}
-
-func CoreToRegVal(data auth.Core) UserValidate {
-	return UserValidate{
-		Name:     data.Name,
-		Email:    data.Email,
-		Password: data.Password,
-	}
-}
-func RegistrationValidate(data auth.Core) error {
-	validate := validator.New()
-	val := CoreToRegVal(data)
-	if err := validate.Struct(val); err != nil {
-		for _, e := range err.(validator.ValidationErrors) {
-			vlderror := ""
-			if e.Field() == "Password" && e.Value() != "" {
-				vlderror = fmt.Sprintf("%s is not %s", e.Value(), e.Tag())
-				return errors.New(vlderror)
-			}
-			if e.Value() == "" {
-				vlderror = fmt.Sprintf("%s is %s", e.Field(), e.Tag())
-				return errors.New(vlderror)
-			} else {
-				vlderror = fmt.Sprintf("%s is not %s", e.Value(), e.Tag())
-				return errors.New(vlderror)
-			}
-		}
-	}
-	return nil
 }
